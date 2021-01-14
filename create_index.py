6ac46725743ff5e800ea3bff370640b56eb8934f
skjerns <14980558+skjerns@users.noreply.github.com>
2021-01-14 10:42:36 +0100

    relative names

--- create_index.py
+++ create_index.py
@@ -26,15 +26,16 @@ from mako.template import Template
 
 def main(root):
     folders = ospath.list_folders(root, subfolders=True, add_parent=True)
+    folders = [f for f in folders if not '.git' in f]
     for folder in folders:
-        if folder=='.git': continue
+        if '.git' in folder: continue
         html = index(folder)
         with open(folder + '/index.html', 'w') as f:
             f.write(html)
 
 def index(folder):
-
-    fnames = [fname for fname in sorted(folder)   if fname not in EXCLUDED]
+    files = ospath.list_files(folder, relative=True)
+    fnames = [fname for fname in sorted(files) if fname not in EXCLUDED]
     header = os.path.basename(folder)
     html = Template(INDEX_TEMPLATE).render(names=fnames, header=header)
     return html
