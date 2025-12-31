1e632a3715cf0f3c05a0cc8b3a1f0d9d27bebfe1
Simon Kern <14980558+skjerns@users.noreply.github.com>
2025-12-31 14:05:53 +0100

    update to 2026

--- code/commons.py
+++ code/commons.py
@@ -13,6 +13,8 @@ from tqdm import tqdm
 from joblib.memory import Memory
 mem = Memory('.')
 
+YEAR = 2026
+
 @mem.cache
 def get_street_coords(street):
     street = street.upper()
@@ -22,10 +24,10 @@ def get_street_coords(street):
     if os.path.isfile(streets_json):
         with open(streets_json, 'r') as f:
             coords = json.load(f)
-            
-    if street in coords: 
+
+    if street in coords:
         return coords[street]
-    
+
     geolocator = Nominatim(user_agent=f"sperrmuell_ka_{street}")
     loc = geolocator.geocode({'street':street, 'city': 'Karlsruhe', 'country':'Germany'})
     if loc is not None:
@@ -36,10 +38,10 @@ def get_street_coords(street):
         json.dump(coords, f, indent=4, ensure_ascii=False)
     time.sleep(0.5)
     return coords[street]
-    
+
 def multiple_replace(dict, text):
   # Create a regular expression  from the dictionary keys
-  #https://stackoverflow.com/questions/15175142/how-can-i-do-multiple-substitutions-using-regex  
+  #https://stackoverflow.com/questions/15175142/how-can-i-do-multiple-substitutions-using-regex
   regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
   # For each match, look-up corresponding value in dictionary
-  return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text) 
+  return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
